a7c5708090f5cd096ccd60ad3decd0272f7670ee
Felipe Ramirez <framirez@bwl.com.mx>
2023-09-09 22:26:38 -0500

    feat: :art: Add clerk and ui libary

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+  return <div>unprotected page landing</div>;
+};
+
+export default LandingPage;
---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'bun:test';
+
+describe('index', () => {
+  it('should work', () => {
+    expect(true).toBe(true);
+    // throw new Error('This is not working');
+  })
+})
